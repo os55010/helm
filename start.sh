fc9aadbe303afb7256762d7b8076c11786e1f1f5
os55010 <os55010@gmail.com>
2020-01-02 17:55:43 +0900

    stable add

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
-wget https://get.helm.sh/helm-v3.0.2-linux-386.tar.gz
-tar xvfz helm-v3.0.2-linux-386.tar.gz
-sudo cp linux-386/helm /usr/local/bin/
+if [[ ! -e /usr/local/bin/helm ]]; then
+  wget https://get.helm.sh/helm-v3.0.2-linux-386.tar.gz
+  tar xvfz helm-v3.0.2-linux-386.tar.gz
+  sudo cp linux-386/helm /usr/local/bin/
+fi
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
